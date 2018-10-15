a436a3d1654dcebc61889b0537483ef3982e459f
Aviral-Sharma-10 <100aviral100@gmail.com>
2018-10-15 17:32:20 +0530

    Fibonacci series generator added

--- build.py
+++ build.py
@@ -1,1 +1,14 @@
+"""Program to generate Fibonacci sequence"""
 
+n1,n2,n3 = 1,0,0
+fib=[]
+
+n=int(input("Enter number of terms : "))
+
+while(n>0):
+    fib.append(n3)
+    n3 = n1+n2
+    n1, n2 = n2, n3
+    n-=1
+
+print(fib)
